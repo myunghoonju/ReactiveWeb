593c5298332ca04cbce4d1293e21e2f728e79c15
MYUNGHOON JU <myunghoonju@gmail.com>
2022-08-13 16:20:11 +0900

    tidy up import

--- src/test/java/practice/reactiveWeb/testcontainers/RabbitMQ.java
+++ src/test/java/practice/reactiveWeb/testcontainers/RabbitMQ.java
@@ -1,6 +1,5 @@
 package practice.reactiveWeb.testcontainers;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
